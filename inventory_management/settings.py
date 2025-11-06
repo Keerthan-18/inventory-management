df246e0686eb25389c5a8a02fadbd8612440c5cb
Vijay Keeerthan <vijaykeerthan585@gmail.com>
2025-11-06 17:23:44 +0530

    fixed .env ALLOWED_HOSTS and DEBUG for deployment

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False")=="True"
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
